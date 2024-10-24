a6862733ad06db0480c8399e986dcfac27688372
Khet <pb@Khets-Macbook.local>
2024-10-24 18:18:17 +0700

    feat: implement select fruits and vegetable

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,138 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useRef, useState } from 'react'
+
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+
+import { ITEMS, ItemsType } from '../consts/Items'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  const [items, setItems] = useState(ITEMS)
+  const [fruitsSelected, setFruitsSelected] = useState<ItemsType[]>([])
+  const [vegetableSelected, setVegetableSelected] = useState<ItemsType[]>([])
+  const selectedTimeoutRef = useRef<Record<string, NodeJS.Timeout>>({})
+
+  const handleClickItems = ({ name, type }: ItemsType) => {
+    switch (type) {
+      case 'Fruit':
+        setFruitsSelected((prev) => [...prev, { name, type }])
+        selectedTimeoutRef.current[name] = setTimeout(() => {
+          setItems((prev) => [...prev, { name, type }])
+          setFruitsSelected((prev) => prev.filter((i) => i.name !== name))
+        }, 5000)
+        break
+      case 'Vegetable':
+        setVegetableSelected((prev) => [...prev, { name, type }])
+        selectedTimeoutRef.current[name] = setTimeout(() => {
+          setItems((prev) => [...prev, { name, type }])
+          setVegetableSelected((prev) => prev.filter((i) => i.name !== name))
+        }, 5000)
+        break
+    }
+    setItems((prev) => {
+      const newPrev = prev.filter((i) => i.name !== name)
+      return newPrev
+    })
+  }
+
+  const handleGetBackItems = ({ name, type }: ItemsType) => {
+    switch (type) {
+      case 'Fruit':
+        setFruitsSelected((prev) => prev.filter((i) => i.name !== name))
+        break
+      case 'Vegetable':
+        setVegetableSelected((prev) => prev.filter((i) => i.name !== name))
+        break
+    }
+    if (selectedTimeoutRef.current && selectedTimeoutRef.current[name]) {
+      clearTimeout(selectedTimeoutRef.current[name])
+    }
+    setItems((prev) => [...prev, { name, type }])
+  }
+
+  return (
+    <Container maxWidth="lg" sx={containerBox}>
+      <Box display="flex" columnGap="1rem" height="100%">
+        <Box sx={{ width: '100%' }} flexDirection="column" display="flex" rowGap={2}>
+          {items.map((item) => (
+            <Box key={`${item?.name}-items`}>
+              <Button
+                variant="outlined"
+                color={"#fff" as any}
+                fullWidth
+                onClick={() => handleClickItems(item)}
+              >
+                {item?.name}
+              </Button>
+            </Box>
+          ))}
+        </Box>
+        <Box sx={selectedBox}>
+          <Box width="100%">
+            <Box sx={selectedHeaderBox}>
+              Fruit
+            </Box>
+            <Box flexDirection="column" display="flex" rowGap={2} padding={1}>
+              {fruitsSelected.map((item) => (
+                <Box key={`${item?.name}-fruitsSelected`}>
+                  <Button
+                    variant="outlined"
+                    color={"#fff" as any}
+                    fullWidth
+                    onClick={() => handleGetBackItems(item)}
+                  >
+                    {item?.name}
+                  </Button>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+        <Box sx={selectedBox}>
+          <Box width="100%">
+            <Box sx={selectedHeaderBox}>
+              Vegetable
+            </Box>
+            <Box flexDirection="column" display="flex" rowGap={2} padding={1}>
+              {vegetableSelected.map((item) => (
+                <Box key={`${item?.name}-vegetableSelected`}>
+                  <Button
+                    variant="outlined"
+                    color={"#fff" as any}
+                    fullWidth
+                    onClick={() => handleGetBackItems(item)}
+                  >
+                    {item?.name}
+                  </Button>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
+
+
+const selectedBox = {
+  width: '100%',
+  display: 'flex',
+  border: '1px solid black'
+} as const
+
+const selectedHeaderBox = {
+  backgroundColor: '#f0f0f0',
+  height: 'fit-content',
+  padding: 1,
+  textAlign: 'center',
+  borderBottom: '1px solid black'
+} as const
+
+const containerBox = {
+  py: 4,
+  height: '100%'
+} as const
